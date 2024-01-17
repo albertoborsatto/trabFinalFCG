3ce783aa67a683b3113657afa91b4b5c14f905d3
quinhas123 <lucasmschirmbeck@gmail.com>
2024-01-17 20:46:46 -0300

    first commit.

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/glad.c.obj"
+  "CMakeFiles/main.dir/src/glad.c.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj.d"
+  "D:/Laboratorio_4_Codigo_Fonte/bin/Debug/main.exe"
+  "D:/Laboratorio_4_Codigo_Fonte/bin/Debug/main.exe.manifest"
+  "D:/Laboratorio_4_Codigo_Fonte/bin/Debug/main.pdb"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
